d50f587a38c04bb03fd9f247b6efb6ffaebc8fd1
Setra Rakotovao <setra.rakotovao@student-cs.fr>
2025-03-03 00:21:14 +0100

    make paddles move

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from kivy.uix.widget import Widget
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.core.window import Window
 
 
 class PongBall(Widget):
-    velocity = Vector(3, 4)
+    velocity = Vector(4, 4)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -60,6 +61,11 @@ class PongGame(Widget):
         self.add_widget(self.paddle_left)
         self.add_widget(self.paddle_right)
 
+        # Track pressed keys
+        self.pressed_keys = set()
+        Window.bind(on_key_down=self.on_key_down)
+        Window.bind(on_key_up=self.on_key_up)
+
         Clock.schedule_interval(self.update, 1.0 / 60.0)  # 60 FPS
 
     def on_size(self, *args):
@@ -70,8 +76,32 @@ class PongGame(Widget):
         self.paddle_left.pos = (10, self.height / 2 - self.paddle_left.height / 2)
         self.paddle_right.pos = (self.width - 20, self.height / 2 - self.paddle_right.height / 2)
 
+    def on_key_down(self, window, key, *args):
+        self.pressed_keys.add(key)
+
+    def on_key_up(self, window, key, *args):
+        self.pressed_keys.discard(key)
+
+    def move_paddles(self, dt):
+        step = 10  # Paddle movement speed
+
+        # Move Player 1 (Left Paddle) - Z (up) & S (down)
+        if 122 in self.pressed_keys:  # 'z' key
+            self.paddle_left.y = min(self.height - self.paddle_left.height, self.paddle_left.y + step)
+        if 115 in self.pressed_keys:  # 's' key
+            self.paddle_left.y = max(0, self.paddle_left.y - step)
+
+        # Move Player 2 (Right Paddle) - Up & Down arrows
+        if 273 in self.pressed_keys:  # Up arrow
+            self.paddle_right.y = min(self.height - self.paddle_right.height, self.paddle_right.y + step)
+        if 274 in self.pressed_keys:  # Down arrow
+            self.paddle_right.y = max(0, self.paddle_right.y - step)
+
     def update(self, dt):
         self.ball.move()
+
+        # Move paddles
+        self.move_paddles(dt)
 
         # Check for paddle collisions
         self.paddle_left.bounce_ball(self.ball)
